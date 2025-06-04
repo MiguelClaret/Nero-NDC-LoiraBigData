c5220a64e72df525cb32e3c8daec9d4010536b1a
MariMari0945 <mariana.souza@sou.inteli.edu.br>
2025-06-04 10:07:27 -0300

    feat: final auditor detaisl

--- src/front-end/src/utils/userOp/auditHarvestUserOp.js
+++ src/front-end/src/utils/userOp/auditHarvestUserOp.js
@@ -1,5 +1,4 @@
 import { ethers } from "ethers";
-import HarvestManagerAbi from "../../abi/abiHarvest.json";
 import { getUserOperationClient } from "./userOpClient";
 import { getSimpleAccountBuilder } from "./userOpBuilder";
 import { CONTRACT_ADDRESSES, AA_PLATFORM_CONFIG } from "../../config/neroConfig";
@@ -15,7 +14,9 @@ export const auditHarvestUserOp = async (
   }
 ) => {
   try {
-    const contractAddress = CONTRACT_ADDRESSES.harvestManager;
+    const contractAddress = CONTRACT_ADDRESSES.harvestManager.address;
+    const contractAbi = CONTRACT_ADDRESSES.harvestManager.abi;
+
     const client = await getUserOperationClient();
     const builder = await getSimpleAccountBuilder(signer);
 
@@ -27,18 +28,23 @@ export const auditHarvestUserOp = async (
       paymasterAddress: AA_PLATFORM_CONFIG.paymasterAddress,
     });
 
-    builder.setCallGasLimit(AA_PLATFORM_CONFIG.defaultGasLimit);
-    builder.setVerificationGasLimit(AA_PLATFORM_CONFIG.defaultVerificationGasLimit);
-    builder.setPreVerificationGas(AA_PLATFORM_CONFIG.defaultPreVerificationGas);
+    builder.setCallGasLimit(800_000);
+    builder.setVerificationGasLimit(500_000);
+    builder.setPreVerificationGas(50_000);
 
-    const contract = new ethers.Contract(contractAddress, HarvestManagerAbi);
+    const contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
     let calldata;
+
     if (action === "approve") {
+      if (!ethers.utils.isAddress(toAddress)) {
+        throw new Error(`Endereço inválido: ${toAddress}`);
+      }
+
       calldata = contract.interface.encodeFunctionData("mintHarvest", [
         toAddress,
         harvestId,
-        ethers.utils.parseUnits(String(tco2Amount), 18),
+        ethers.utils.parseUnits(String(tco2Amount).replace(",", "."), 18)
       ]);
     } else if (action === "reject") {
       calldata = contract.interface.encodeFunctionData("rejectHarvest", [harvestId]);
@@ -49,6 +55,8 @@ export const auditHarvestUserOp = async (
     const userOp = await builder.execute(contractAddress, 0, calldata);
     const res = await client.sendUserOperation(userOp);
     const receipt = await res.wait();
+
+    console.log("Safra auditada. Tx Hash:", receipt.transactionHash);
     return receipt.transactionHash;
   } catch (err) {
     console.error("Erro ao auditar safra:", err);
