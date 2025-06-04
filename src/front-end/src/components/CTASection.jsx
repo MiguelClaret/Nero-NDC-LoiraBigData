ea72b4a868a665737dca7827d9219b4b4983eedc
Ian Pereira Simão <159059598+ianpsa@users.noreply.github.com>
2025-06-03 22:16:24 -0300

    fix: adds fix on broken website

--- src/front-end/src/components/CTASection.jsx
+++ src/front-end/src/components/CTASection.jsx
@@ -8,13 +8,11 @@ const CTASection = ({ openWalletModal }) => {
     <section className="py-12 px-8 bg-gray-100 text-center">
       <div className="max-w-2xl mx-auto mb-12">
         <h2 className="text-3xl font-bold mb-4">Ready to revolutionize agriculture?</h2>
-        <h2 className="text-3xl font-bold mb-4">Ready to revolutionize agriculture?</h2>
         <p className="text-lg mb-8">
           Join hundreds of producers and investors on the platform that is transforming sustainable agricultural
           financing
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center mb-16">
-          <button
           <button
             onClick={openWalletModal}
             className="py-4 px-8 rounded-md text-lg font-semibold bg-green-700 text-white hover:bg-green-800 hover:-translate-y-0.5 transition-all"
